6b8348cd64bfc82918dfc4151c6e2b94d07d8783
ludmila <gnydynluda@gmail.com>
2022-12-25 18:52:47 +0100

    first

---
+++ lesson19.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивилися?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false   
+
+};
+
+const a = prompt('Один із останніх переглянутих фільмів?', ''),
+      b = prompt('На скільки оціните його?', ''),
+      c = prompt('Один із останніх переглянутих фільмів?', ''),
+      d = prompt('На скільки оціните його?', '');
+
+personalMovieDB.movies[a] =  b;     
+personalMovieDB.movies[c] =  d;  
+
+console.log(personalMovieDB);
